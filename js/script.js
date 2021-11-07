c9fa95ac957af9cc73e7c74b8f785d30210fa584
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2021-11-07 19:27:39 +0300

    commit7112

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@
 $(document).ready(function () { 
     $('.header__burger').click(function (Event) { 
         $('.header__burger, .header__menu').toggleClass('active'); 
+        $ ('body').toggleClass('lock');
     }); 
 });
